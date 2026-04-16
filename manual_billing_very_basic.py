ce13af96589cc48642b6c7f69c99bc345b2be6b2
tolearnisthegoal <221804288+tolearnisthegoal@users.noreply.github.com>
2026-04-16 21:15:38 +0500

    Manual_billing_very_basic

--- manual_billing_very_basic.py
+++ manual_billing_very_basic.py
@@ -1,22 +1,56 @@
 print('The Smart food')
+
+
+item_chips = 'Chips'
+item_chips_p = 150
+
+item_shawarma = 'Shawarma'
+item_shawarma_p = 250
+
+item_zinger = 'Zinger'
+item_zinger_p = 450
+
+item_coke = 'Coke'
+item_coke_p = 750
+
+item_pasta = 'Pasta'
+item_pasta_p = 900
+
+
+print('-----------------')
 customer_1 = 'Ali'
-items_ali = '1-Chips, 1-Shawarma, 1-Zinger'
-bill_1 = 2000
+print(customer_1)
+print('---')
+print(1, item_chips, 1, item_shawarma, 1, item_zinger)
 
+bill_1 = item_chips_p + item_shawarma_p + item_zinger_p
+print(bill_1)
+
+
+print('-----------------')
 customer_2 = 'Sara'
-items_sara = '1-Pasta, 1-Coke, 5-Glass'
-bill_2 = 1000
+print(customer_2)
+print('---')
+print(3, item_pasta, 5, item_zinger)
+bill_2 = (3*item_pasta_p) + (5 * item_zinger_p)
+print(bill_2)
 
+
+print('-----------------')
 customer_3 = 'Georgia'
-items_georgia = '2-Pasta, 3-Zinger, 3-Regular-Coke, 5-Pizza'
-bill_3 = 10000
+print(customer_3)
+print('---')
+print(5, item_pasta, 5, item_coke)
+bill_3 = (5* item_pasta_p) + (5 * item_coke_p)
+print(bill_3)
 
-total = bill_1 + bill_2 + bill_3
+print('--!+-*/%---')
+
+print('-----------------')
 
 print('Total Revenue Today')
 
-print(customer_1, bill_1 , items_ali, sep=' ------ ')
-print(customer_2, bill_2, items_sara, sep=' ------ ')
-print(customer_3, bill_3, items_georgia, sep=' ----  ')
-print('_--_+-*/%_--_')
-print("Total", total, sep='~~~~~~~~~~~~~~')
+total_revenue = bill_1 + bill_2 + bill_3
+
+print(total_revenue)
+
