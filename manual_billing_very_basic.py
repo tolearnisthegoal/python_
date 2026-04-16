d0c59ecc76fbcf161a9ab921a3f927bdb0271229
tolearnisthegoal <221804288+tolearnisthegoal@users.noreply.github.com>
2026-04-16 20:54:44 +0500

    Manual billing very basic

--- manual_billing_very_basic.py
+++ manual_billing_very_basic.py
@@ -1,18 +1,22 @@
 print('The Smart food')
 customer_1 = 'Ali'
-customer_2 = 'Sara'
-customer_3 = 'Georgia'
-
+items_ali = '1-Chips, 1-Shawarma, 1-Zinger'
 bill_1 = 2000
+
+customer_2 = 'Sara'
+items_sara = '1-Pasta, 1-Coke, 5-Glass'
 bill_2 = 1000
+
+customer_3 = 'Georgia'
+items_georgia = '2-Pasta, 3-Zinger, 3-Regular-Coke, 5-Pizza'
 bill_3 = 10000
 
 total = bill_1 + bill_2 + bill_3
 
-print('-------------------------')
+print('Total Revenue Today')
 
-print(customer_1, bill_1 , sep='        ---     ')
-print(customer_2, bill_2, sep='       ---     ')
-print(customer_3, bill_3, sep='    ---     ')
-print('-------------------------')
+print(customer_1, bill_1 , items_ali, sep=' ------ ')
+print(customer_2, bill_2, items_sara, sep=' ------ ')
+print(customer_3, bill_3, items_georgia, sep=' ----  ')
+print('_--_+-*/%_--_')
 print("Total", total, sep='~~~~~~~~~~~~~~')
