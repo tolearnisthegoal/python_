15bcd74094b3c0852824db811b5395c58350dc65
tolearnisthegoal <221804288+tolearnisthegoal@users.noreply.github.com>
2026-04-16 20:40:52 +0500

    Second day, Billing manual

---
+++ manual_billing_very_basic.py
@@ -0,0 +1,18 @@
+print('The Smart food')
+customer_1 = 'Ali'
+customer_2 = 'Sara'
+customer_3 = 'Georgia'
+
+bill_1 = 2000
+bill_2 = 1000
+bill_3 = 10000
+
+total = bill_1 + bill_2 + bill_3
+
+print('-------------------------')
+
+print(customer_1, bill_1 , sep='        ---     ')
+print(customer_2, bill_2, sep='       ---     ')
+print(customer_3, bill_3, sep='    ---     ')
+print('-------------------------')
+print("Total", total, sep='~~~~~~~~~~~~~~')
